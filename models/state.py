55afb184482278abc5298e2aa44c8918dbab83ae
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-19 23:04:09 +0100

    fix errors

--- models/state.py
+++ models/state.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state")
-        cities = relationship("city", cascade="all, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """Getter attribute that returns the list of City instances
-            with state_id equals to the current State.id
-            """
-            city_list = []
-            all_cities = models.storage.all("City")
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    name = ""
