69077af6391dfe0f54205ea099f93d24e68b00ab
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-20 22:48:59 +0100

    fix errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenity class"""
+    name = Column(String(128), nullable=False)
