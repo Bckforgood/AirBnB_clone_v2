3d7d8279ba6144537d169cd0eebe56605ab7822a
Bckforgood <t.abdo.khbaza@gmail.com>
2023-10-07 08:21:29 -0700

    testpush

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install nginx
 # Create necessary directories and symbolic link
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "Hello, this is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html> <head> </head> <body> Holberton School </body> </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user and group
