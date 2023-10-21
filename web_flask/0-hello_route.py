75170e4853ba0496eb2c09431846aabd5ff80e8e
Bckforgood <t.abdo.khbaza@gmail.com>
2023-10-21 16:56:11 -0700

    chmod

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
